95f51401612a4dd1c3e6a873aad89d861dffbe99
Jason Peng <jason.peng.2019@gmail.com>
2025-06-16 01:22:55 -0500

    WIP SideQuestBootloader.c v0.00

--- Drivers/UART/UART.c
+++ Drivers/UART/UART.c
@@ -1,5 +1,5 @@
 #include "stdio.h"
-#include "usart.h"  // Include the UART handle
+#define HAL_MAX_DELAY 1000
 
 int __io_putchar(int ch) {
     HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
