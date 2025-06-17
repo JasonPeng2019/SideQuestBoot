7ebe96ce3d61dde2701b508025311019550b7ac5
JasonPeng2019 <jason.peng.2019@gmail.com>
2025-06-17 17:15:08 -0500

    Fix bootloader compile issues

--- Core/Inc/SideQuestBootloader.h
+++ Core/Inc/SideQuestBootloader.h
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include "stm32l4xx_hal.h"
-#include "../Drivers/EEPROM/EEPROM.h"
-#include "../Drivers/UART/UART.c"
+#include "../../Drivers/EEPROM/EEPROM.h"
+#include "../../Drivers/UART/UART.c"
 #include "flags.h"
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 
 
@@ -23,6 +24,9 @@
 #define UPDATE_IMAGE_START      ((uint32_t)&__update_img_start__) // fill in here
 #define STABLE_IMAGE_START       ((uint32_t)&__backup_app_start__)// fill in here
 
+
+extern uint32_t __update_img_start__;
+extern uint32_t __backup_app_start__;
 
 
 #define EOF_MARKER                   0xDEADBEEF
@@ -45,9 +49,14 @@ typedef struct {
     I2C_HandleTypeDef   Boot_I2C_Handle;
 } tBootloader;
 
+typedef void (*pFunction)(void);
+
 
 void Bootloader_init(I2C_HandleTypeDef i2c_handle, UART_HandleTypeDef UART_Handle);
-void SideQuestBootloader(void); 
+void SideQuestBootloader(void);
+bool generate_random_bytes(uint8_t *buffer, uint32_t length);
+void jump_to_app(uint32_t address_start);
+bool Valid_FW_Check(void);
 
 
 
--- Core/Src/SideQuestBootloader.c
+++ Core/Src/SideQuestBootloader.c
@@ -8,8 +8,11 @@ SideQuestBootloader.c v0.00
 #define DEBUG_MODE
 
 #include "../Inc/SideQuestBootloader.h"
-#include "main.c"
+#include "main.h"
 #include <stm32l476xx.h>
+
+extern CRC_HandleTypeDef hcrc;
+extern RNG_HandleTypeDef hrng;
 
 //to do: set IWDG script, link IWDG
 // need to prepad the block with 0's at the end when saving from BLE
@@ -18,10 +21,10 @@ static uint32_t firmware_address;
 //pointer at the address in src we have successfully copied`
 static uint32_t * pSuccess_read_marker;
 //pointer at the address of dest we have successfully copied
-static uint32_t * pSucess_write_marker;
-Bootstate SideQuest_State;
+static uint32_t * pSuccess_write_marker;
+BootState SideQuest_State;
 
-tBootloader *SideQuest
+tBootloader *SideQuest;
 
 void SideQuestBootloader(void){    
     
@@ -29,36 +32,36 @@ void SideQuestBootloader(void){
 
     case STATE_INIT: {
         if (__HAL_RCC_GET_FLAG(RCC_FLAG_IWDGRST)){
-            bool crash_Flag = CRASHED;
-            EEPROM_Write_Flag(crash_Flag, PAGE_CRASH_FLAG, SideQuest->Boot_I2C_Handle);
+            uint8_t crash_Flag = CRASHED;
+            EEPROM_Write_Flag(&crash_Flag, PAGE_CRASH_FLAG, &SideQuest->Boot_I2C_Handle);
         } else {break;}
 
-        bool LP_flag;
-        if (EEPROM_Read_Flag(&LP_flag, PAGE_LP_FLAG, SideQuest->Boot_I2C_Handle)){
+        uint8_t LP_flag;
+        if (EEPROM_Read_Flag(&LP_flag, PAGE_LP_FLAG, &SideQuest->Boot_I2C_Handle)){
             if (LP_flag == LOW_POWER){
                 printf("Low Power. Jumping to app");
                 jump_to_app(FLASH2_START);
             }
         } else {break;}
 
-        bool * id_flag_data;
-        if (EEPROM_ReadByte(PAGE_ID, id_flag_data, SideQuest->Boot_I2C_Handle)){
-            if (*id_flag_data == false){
+        uint8_t id_flag_data;
+        if (EEPROM_ReadByte(PAGE_ID, &id_flag_data, &SideQuest->Boot_I2C_Handle)){
+            if (id_flag_data == 0){
                 uint8_t random_bytes[30];
-                if (generate_random_bytes(random_bytes)){
+                if (generate_random_bytes(random_bytes, sizeof(random_bytes))){
                     uint16_t start_address = PAGE_ID;
-                    bool new_flag = true;
-                    EEPROM_WriteByte(start_address, new_flag, SideQuest->Boot_I2C_Handle);
+                    uint8_t new_flag = 1;
+                    EEPROM_WriteByte(start_address, new_flag, &SideQuest->Boot_I2C_Handle);
                     start_address += 1;
-                    EEPROM_WritePage(start_address, random_bytes, 30, SideQuest->Boot_I2C_Handle);
+                    EEPROM_WritePage(start_address, random_bytes, 30, &SideQuest->Boot_I2C_Handle);
                 } else {break;}
             }
         } else {break;}
 
-        bool * crashed_flag;
-        if (EEPROM_Read_Flag(crashed_flag, PAGE_CRASH_FLAG, SideQuest->Boot_I2C_Handle)){
+        uint8_t crashed_flag;
+        if (EEPROM_Read_Flag(&crashed_flag, PAGE_CRASH_FLAG, &SideQuest->Boot_I2C_Handle)){
             if (crashed_flag == CRASHED){
-                SideQuest_State = STATE_STARTING_READ_FROM_STABLE
+                SideQuest_State = STATE_STARTING_READ_FROM_STABLE;
             } else {
                 SideQuest_State = STATE_VERIFY_UPDATE;
             }
@@ -68,8 +71,8 @@ void SideQuestBootloader(void){
     }
 
     case STATE_VERIFY_UPDATE: {
-        bool * update_ready_flag;
-        if (EEPROM_Read_Flag(PAGE_UPDATE_FLAG, update_ready_flag, SideQuest->Boot_I2C_Handle)){
+        uint8_t update_ready_flag;
+        if (EEPROM_Read_Flag(&update_ready_flag, PAGE_UPDATE_FLAG, &SideQuest->Boot_I2C_Handle)){
             if (update_ready_flag == UPDATE_NEEDED) {
                 SideQuest_State = STATE_CHECK_FW;
             } else {
@@ -81,18 +84,18 @@ void SideQuestBootloader(void){
     }
 
     case STATE_CHECK_FW:{
-        bool * update_flag;
-        if (EEPROM_Read_Flag(update_flag, PAGE_UPDATE_FLAG, SideQuest->Boot_I2C_Handle)){
+        uint8_t update_flag;
+        if (EEPROM_Read_Flag(&update_flag, PAGE_UPDATE_FLAG, &SideQuest->Boot_I2C_Handle)){
             if (update_flag == UPDATE_NEEDED){
                 uint8_t fw_header[30];
                 memcpy(fw_header, UPDATE_IMAGE_START, 30);
                 uint8_t id_header[30];
-                if (EEPROM_Read(PAGE_ID + 1, id_header, 30, SideQuest->Boot_I2C_Handle)){
-                    if (fw_header == id_header){
+                if (EEPROM_Read(PAGE_ID + 1, id_header, 30, &SideQuest->Boot_I2C_Handle)){
+                    if (memcmp(fw_header, id_header, sizeof(fw_header)) == 0){
                         firmware_address = UPDATE_IMAGE_START;
                         SideQuest_State = STATE_ERASE_FLASH;
                     } else {
-                        SideQuest_State = STATE_JUMP_TO_APP
+                        SideQuest_State = STATE_JUMP_TO_APP;
                     }
                 } else {break;}
             }
@@ -112,7 +115,7 @@ void SideQuestBootloader(void){
             HAL_FLASH_Unlock(); 
             uint32_t copy_firmware_addr = firmware_address;
             pSuccess_read_marker = (uint32_t *)copy_firmware_addr;
-            pSucess_write_marker = (uint32_t *)FLASH2_START;
+            pSuccess_write_marker = (uint32_t *)FLASH2_START;
 
             SideQuest_State = STATE_MOVING_BLOCK;
         } else {
@@ -137,17 +140,17 @@ void SideQuestBootloader(void){
             //make a copy of pfirmware_success_marker and use it to get the next 64 bits of data
             uint32_t * copy_psuccess_marker = pSuccess_read_marker;
             uint64_t data64 = ((uint64_t)copy_psuccess_marker[1] << 32) | copy_psuccess_marker[0];
-            if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, pSucess_write_marker, data64);) {
+            if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, (uint32_t)pSuccess_write_marker, data64) == HAL_OK) {
                 uint32_t crc1 = HAL_CRC_Accumulate(&hcrc, pSuccess_read_marker, (BLOCKSIZE / 8) / (sizeof(uint32_t)));
-                uint32_t crc2 = HAL_CRC_Accumulate(&hcrc, pSucess_write_marker, (BLOCKSIZE / 8) / (sizeof(uint32_t)));
+                uint32_t crc2 = HAL_CRC_Accumulate(&hcrc, pSuccess_write_marker, (BLOCKSIZE / 8) / (sizeof(uint32_t)));
                 #ifdef DEBUG_MODE
                     uint32_t crc1 = HAL_CRC_Calculate(&hcrc, pSuccess_read_marker, (BLOCKSIZE / 8) / (sizeof(uint32_t)));
-                    uint32_t crc2 = HAL_CRC_Calculate(&hcrc, pSucess_write_marker, (BLOCKSIZE / 8) / (sizeof(uint32_t)));
+                    uint32_t crc2 = HAL_CRC_Calculate(&hcrc, pSuccess_write_marker, (BLOCKSIZE / 8) / (sizeof(uint32_t)));
                 #endif
                 if (crc1 == crc2) {
                     // if crc check passes increment pointers in each flash
                     pSuccess_read_marker += (BLOCKSIZE / (sizeof(uint32_t)));
-                    pSucess_write_marker += (BLOCKSIZE / (sizeof(uint32_t)));
+                    pSuccess_write_marker += (BLOCKSIZE / (sizeof(uint32_t)));
                     success = true;
                 }
             }
@@ -165,10 +168,10 @@ void SideQuestBootloader(void){
     case STATE_FINAL_CHECKSUM: {
         HAL_FLASH_Lock();  // Done writing
         uint32_t crc1 = HAL_CRC_Calculate(&hcrc, pSuccess_read_marker, (BLOCKSIZE / 8) / (sizeof(uint32_t)));
-        uint32_t crc2 = HAL_CRC_Calculate(&hcrc, pSucess_write_marker, (BLOCKSIZE / 8) / (sizeof(uint32_t)))
+        uint32_t crc2 = HAL_CRC_Calculate(&hcrc, pSuccess_write_marker, (BLOCKSIZE / 8) / (sizeof(uint32_t)));
         if (crc1 == crc2) {
-            bool good_to_go = GOOD_TO_GO;
-            EEPROM_Write_Flag(good_to_go, PAGE_UPDATE_FLAG, SideQuest->Boot_I2C_Handle)
+            uint8_t good_to_go = GOOD_TO_GO;
+            EEPROM_Write_Flag(&good_to_go, PAGE_UPDATE_FLAG, &SideQuest->Boot_I2C_Handle);
         } else {
             SideQuest_State = STATE_ERROR;
         }
@@ -224,22 +227,22 @@ void Bootloader_init(I2C_HandleTypeDef i2c_handle, UART_HandleTypeDef UART_Handl
     SideQuest = (tBootloader *)malloc(sizeof(tBootloader));
     SideQuest->Boot_I2C_Handle = i2c_handle;
     SideQuest->Boot_UART_Handle = UART_Handle;
-    UART_SetHandle(SideQuest->Boot_UART_Handle);
+    UART_SetHandle(&SideQuest->Boot_UART_Handle);
     SideQuest_State = STATE_INIT;
 }
 
-void jump_to_app(uint32_t * address_start){
-    if (Valid_FW_Check){
-        HAL_UART_DeInit(SideQuest->Boot_I2C_Handle);
-        HAL_UART_DeInit(SideQuest->Boot_UART_Handle);
-        HAL_CRC_DeInit(hcrc);
+void jump_to_app(uint32_t address_start){
+    if (Valid_FW_Check()){
+        HAL_I2C_DeInit(&SideQuest->Boot_I2C_Handle);
+        HAL_UART_DeInit(&SideQuest->Boot_UART_Handle);
+        HAL_CRC_DeInit(&hcrc);
         HAL_RCC_DeInit();
         HAL_DeInit();
         SysTick->CTRL = 0;
         SysTick->LOAD = 0;
         SysTick->VAL  = 0;
-        uint32_t appStack = *(volatile uint32_t*)FLASH2_START;
-        uint32_t appResetHandler = *(volatile uint32_t*)(FLASH2_START + 4);
+        uint32_t appStack = *(volatile uint32_t*)address_start;
+        uint32_t appResetHandler = *(volatile uint32_t*)(address_start + 4);
 
         __set_MSP(appStack);
 
@@ -254,6 +257,6 @@ void jump_to_app(uint32_t * address_start){
 
 }
 
-void Valid_FW_Check(){
-    return ((((*(uint32_t*)FLASH2_START) - SRAM1_BASE) <= SRAM1_SIZE_MAX)? TRUE : FALSE)
+bool Valid_FW_Check(){
+    return (((*(uint32_t*)FLASH2_START) - SRAM1_BASE) <= SRAM1_SIZE_MAX);
 }
